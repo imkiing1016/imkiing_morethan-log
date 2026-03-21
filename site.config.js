74bf1c4835f8ae9eb4ecdaf024f8dbcc3b83d0a3
imkiing1016 <102226724+imkiing1016@users.noreply.github.com>
2026-03-21 21:48:31 +0900

    fix: CONFIG export 추가하여 빌드 에러 수정

--- site.config.js
+++ site.config.js
@@ -72,9 +72,4 @@ const CONFIG = {
   revalidateTime: 21600 * 7, // revalidate time for [slug], index
 }
 
-module.exports = {
-  rootNotionPageId: "2ee8a4873d11815b9de8c1b26966b3ae?v=2ee8a4873d11815891fa000cc67bd71d&source=copy_link",
-  domain: "morethan-log.vercel.app",
-  name: "나만의 작은 아카이브",
-  author: "imkiing",
-}
+module.exports = { CONFIG }

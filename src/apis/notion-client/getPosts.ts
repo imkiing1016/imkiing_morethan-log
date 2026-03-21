06ebf94aaada7836cbfcff9727893e45f8f43d56
imkiing1016 <102226724+imkiing1016@users.noreply.github.com>
2026-03-21 23:12:33 +0900

    debug: getPosts에 디버그 로그 추가하여 빌드 시 문제 원인 파악

--- src/apis/notion-client/getPosts.ts
+++ src/apis/notion-client/getPosts.ts
@@ -15,45 +15,68 @@ export const getPosts = async () => {
   let id = CONFIG.notionConfig.pageId as string
   const api = new NotionAPI()
 
-  const response = await api.getPage(id)
-  id = idToUuid(id)
-  const collection = Object.values(response.collection)[0]?.value
-  const block = response.block
-  const schema = collection?.schema
+  console.log("[getPosts] pageId:", id)
 
-  const rawMetadata = block[id].value
+  try {
+    const response = await api.getPage(id)
+    console.log("[getPosts] response keys:", Object.keys(response))
+    console.log("[getPosts] collection keys:", Object.keys(response.collection || {}))
+    console.log("[getPosts] block keys count:", Object.keys(response.block || {}).length)
 
-  // Check Type
-  if (
-    rawMetadata?.type !== "collection_view_page" &&
-    rawMetadata?.type !== "collection_view"
-  ) {
-    return []
-  } else {
-    // Construct Data
-    const pageIds = getAllPageIds(response)
-    const data = []
-    for (let i = 0; i < pageIds.length; i++) {
-      const id = pageIds[i]
-      const properties = (await getPageProperties(id, block, schema)) || null
-      // Add fullwidth, createdtime to properties
-      properties.createdTime = new Date(
-        block[id].value?.created_time
-      ).toString()
-      properties.fullWidth =
-        (block[id].value?.format as any)?.page_full_width ?? false
+    id = idToUuid(id)
+    console.log("[getPosts] uuid:", id)
 
-      data.push(properties)
+    const collection = Object.values(response.collection)[0]?.value
+    const block = response.block
+    const schema = collection?.schema
+
+    console.log("[getPosts] collection exists:", !!collection)
+    console.log("[getPosts] schema exists:", !!schema)
+
+    const rawMetadata = block[id]?.value
+    console.log("[getPosts] rawMetadata type:", rawMetadata?.type)
+    console.log("[getPosts] rawMetadata exists:", !!rawMetadata)
+
+    // Check Type
+    if (
+      rawMetadata?.type !== "collection_view_page" &&
+      rawMetadata?.type !== "collection_view"
+    ) {
+      console.log("[getPosts] Type check FAILED - returning empty array")
+      console.log("[getPosts] Available block ids:", Object.keys(block).slice(0, 5))
+      return []
+    } else {
+      // Construct Data
+      const pageIds = getAllPageIds(response)
+      console.log("[getPosts] pageIds count:", pageIds.length)
+
+      const data = []
+      for (let i = 0; i < pageIds.length; i++) {
+        const id = pageIds[i]
+        const properties = (await getPageProperties(id, block, schema)) || null
+        // Add fullwidth, createdtime to properties
+        properties.createdTime = new Date(
+          block[id].value?.created_time
+        ).toString()
+        properties.fullWidth =
+          (block[id].value?.format as any)?.page_full_width ?? false
+
+        data.push(properties)
+      }
+
+      // Sort by date
+      data.sort((a: any, b: any) => {
+        const dateA: any = new Date(a?.date?.start_date || a.createdTime)
+        const dateB: any = new Date(b?.date?.start_date || b.createdTime)
+        return dateB - dateA
+      })
+
+      const posts = data as TPosts
+      console.log("[getPosts] posts count:", posts.length)
+      return posts
     }
-
-    // Sort by date
-    data.sort((a: any, b: any) => {
-      const dateA: any = new Date(a?.date?.start_date || a.createdTime)
-      const dateB: any = new Date(b?.date?.start_date || b.createdTime)
-      return dateB - dateA
-    })
-
-    const posts = data as TPosts
-    return posts
+  } catch (error) {
+    console.error("[getPosts] ERROR:", error)
+    return []
   }
 }

9229072475977c03453c52792257afea50748098
imkiing1016 <102226724+imkiing1016@users.noreply.github.com>
2026-03-22 11:55:27 +0900

    fix: Detail 배경을 전체 화면 너비로 확장하여 좌우 클릭 영역 확보

--- src/routes/Detail/index.tsx
+++ src/routes/Detail/index.tsx
@@ -32,6 +32,8 @@ const StyledWrapper = styled.div`
   padding: 2rem 0;
   min-height: 100vh;
   cursor: pointer;
+  width: 100vw;
+  margin-left: calc(-50vw + 50%);
 
   &[data-type="Paper"] {
     padding: 40px 0;
